1bb0c98cf6832598c5c8f192e85ae9101967677e
nguyenbadat152006-ux <nguyenbadat152006@gmail.com>
2025-12-04 02:14:16 +0700

    Add files via upload

---
+++ bai7_7.py
@@ -0,0 +1,9 @@
+print("Sinh vien: Nguyen Gia Huy")
+print("Ma so SV : 245752021610165")
+print("##########################")
+def count_lines(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return len(file.readlines())
+#vidu
+filename = "C:/Users/khanh/Downloads/New folder/Vanban.txt.txt"
+print("so dong trong tep:", count_lines(filename))
