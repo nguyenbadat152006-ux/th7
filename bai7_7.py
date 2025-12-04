478cc76d0582b713886facd6c23a38345c54b347
nguyenbadat152006-ux <nguyenbadat152006@gmail.com>
2025-12-04 14:53:17 +0700

    Delete bai7_7.py

--- bai7_7.py
+++
@@ -1,8 +0,0 @@
-
-def count_lines(filename):
-    with open(filename, 'r', encoding='utf-8') as file:
-        return len(file.readlines())
-#vidu
-filename = "C:/Users/khanh/Downloads/New folder/Vanban.txt.txt"
-print("so dong trong tep:", count_lines(filename))
-
