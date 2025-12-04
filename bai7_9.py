49401e591d2a737c8e4f3d9cc063e9026d45413c
nguyenbadat152006-ux <nguyenbadat152006@gmail.com>
2025-12-04 12:52:29 +0700

    Update bai7_9.py

--- bai7_9.py
+++ bai7_9.py
@@ -1,7 +1,4 @@
-print("Sinh vien: Nguyen Gia Huy")
-print("Ma so SV : 245752021610165")
-print("##########################")
-# Ten file nguon va file dich
+
 source_file = "Vanban.txt"
 destination_file = "SaoChepVanban.txt"
 
@@ -13,3 +10,4 @@ with open(destination_file, 'w', encoding='utf-8') as dest:
     dest.write(content)  # ghi vao file dich
 
 print(f"Da sao chep noi dung tu '{source_file}' sang '{destination_file}'")
+
